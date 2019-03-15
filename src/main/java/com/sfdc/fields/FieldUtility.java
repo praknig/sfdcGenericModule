3a39a16f34dedb05fb49f8e13e501982d3fd9b83
Prakhar Nigam <prakhartheprakhar@yahoo.com>
2019-03-15 08:48:24 +0530

    added fields utility class

--- src/main/java/com/sfdc/fields/FieldUtility.java
+++ src/main/java/com/sfdc/fields/FieldUtility.java
@@ -1,18 +1,45 @@
 package com.sfdc.fields;
 
+import java.util.HashMap;
+
+import com.google.gson.Gson;
+import com.sfdc.fields.wrappers.*;
+import com.sfdc.util.Constants;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 public class FieldUtility {
 
-	private String fieldAPI,authToken,baseUrl;
-	HashMap<String,>
-	public FieldUtility(String fieldAPIName, String authToken,String baseUrl)
+	private String objectAPI,authToken,baseUrl;
+	HashMap<String,Field> mapFieldAPIAndField;
+	public FieldUtility(String objectAPI, String authToken,String baseUrl)
 	{
 		this.authToken = authToken;
-		this.fieldAPI = fieldAPIName;
+		this.objectAPI = objectAPI;
 		this.baseUrl = baseUrl;
+		mapFieldAPIAndField = new HashMap<String,Field>();
 	}
 	
 	private void fetchFieldDetails()
 	{
-		
+		String url = baseUrl + Constants.SOBJECT_FIELD_API_URL + this.objectAPI + "/describe";
+		Request request = new Request.Builder().url(url).header("Authorization", "Bearer " +this.authToken).build();
+		OkHttpClient client = new OkHttpClient();
+		Gson gson = new Gson();
+		try {
+			Response response = client.newCall(request).execute();
+			String body = response.body().string();
+			Fields fields = (Fields)gson.fromJson(body, Fields.class);
+			parseFields(fields);
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	private void parseFields(Fields fields)
+	{
+		for(Field field:fields.getFields())
+			mapFieldAPIAndField.put(field.getName(), field);
 	}
 }
--- src/main/java/com/sfdc/util/Constants.java
+++ src/main/java/com/sfdc/util/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 	
 	public static final String VERSION_NUMBER =  "44.0";
 	public static final String SOBJECT_API_URL =  "/services/data/"+VERSION_NUMBER+"/sobjects/";
+	public static final String SOBJECT_FIELD_API_URL =  "/services/data/v44.0/sobjects/";
 	
 
 }
